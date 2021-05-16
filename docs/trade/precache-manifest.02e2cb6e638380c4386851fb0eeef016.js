ab01952fafa998b7f628e432d969063d222ff444
muskimum <83860759+muskimum@users.noreply.github.com>
2021-05-16 19:40:32 +0200

    Add files via upload

---
+++ docs/trade/precache-manifest.02e2cb6e638380c4386851fb0eeef016.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee38a612129d25c7783f16dee6c21d21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e5398d984a1565c8779",
+    "url": "./static/css/2.06ebec9a.chunk.css"
+  },
+  {
+    "revision": "31f808a5fbce047fa259",
+    "url": "./static/css/main.8bb63a68.chunk.css"
+  },
+  {
+    "revision": "0e5398d984a1565c8779",
+    "url": "./static/js/2.12fd146d.chunk.js"
+  },
+  {
+    "revision": "d2e09aded9686c343e34a065031febca",
+    "url": "./static/js/2.12fd146d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f808a5fbce047fa259",
+    "url": "./static/js/main.4a6d4587.chunk.js"
+  },
+  {
+    "revision": "8049fe0a1a69c67a7ca5",
+    "url": "./static/js/runtime-main.17bdf1eb.js"
+  },
+  {
+    "revision": "96dfc6998346ab566d4c7b147184fd1c",
+    "url": "./static/media/musk_logo.96dfc699.svg"
+  }
+]);
